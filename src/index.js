6a1c7ed9175562dc945125bdd10411ff32a4d697
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-24 13:41:31 -0600

    completed labwork

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target["new-task-description"].value)
+    form.reset()
+  })
+
 });
+
+function handleToDo (toDo) {
+  console.log(toDo);
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${toDo} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
